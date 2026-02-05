0cd19c94c614b44f414e73905fea50de4106dc85
Sumeet0P <schauhan8886@gmail.com>
2026-02-05 22:24:19 +0700

    Reinitialize repo and add Kubernetes CI/CD project

--- docker-app/app.py
+++ docker-app/app.py
@@ -1,14 +1,15 @@
-import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
-MESSAGE = os.getenv("APP_MESSAGE", "Hello from Docker Compose")
+MESSAGE = os.getenv("APP_MESSAGE", "Hello DevOps")
+SECRET = os.getenv("SECRET_MESSAGE", "No secret")
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(MESSAGE.encode())
+        self.wfile.write(f"{MESSAGE} | {SECRET}".encode())
 
 server = HTTPServer(("0.0.0.0", 8000), Handler)
 print("Server running on port 8000")
